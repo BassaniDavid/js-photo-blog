cc2dd5f36fd9ff534da43760faddc180e9a56677
BassaniDavid <davbassan98@gmail.com>
2025-03-14 15:27:29 +0100

    feat: responsive

--- js/app.js
+++ js/app.js
@@ -12,13 +12,18 @@ axios.get('https://lanciweb.github.io/demo/api/pictures/')
             const { title, url, id } = element
             console.log(title, url, id);
             mainContainer.innerHTML += `<div class="card position-relative">
-                <img src="${(url)}" alt="first image">
+                <img src="${(url)}" alt="immagine numero ${(id)}">
                 <p class="fst-italic">${(title)}</p>
-                <img class="pin position-absolute top-0 start-50 translate-middle" src="img/img/pin.svg" alt="immagine numero ${(id)}">
+                <img class="pin position-absolute top-0 start-50 translate-middle" src="img/img/pin.svg" alt="immagine pin">
             </div>`
         });
 
     })
     .catch(error => {
         console.error(error)
+        mainContainer.innerHTML += `<div class="alert alert-danger" role="alert">
+       Opsy! there seems to be a problem in the code!
+       here are the details:
+       ${error}
+      </div>`
     })
