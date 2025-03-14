92db5148db711014aca89c71d8b5a6ce7380a3ef
BassaniDavid <davbassan98@gmail.com>
2025-03-14 14:58:05 +0100

    feat: milestone3 ver.1 complete

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
-console.log('ciao')
+
+// richiamo contenitore card dall html
+const mainContainer = document.getElementById('main-container')
+console.log(mainContainer)
+
+// ajax
+axios.get('https://lanciweb.github.io/demo/api/pictures/')
+    .then(response => {
+        const imgArray = response.data
+        console.log(imgArray)
+        imgArray.forEach(element => {
+            const { title, url, id } = element
+            console.log(title, url, id);
+            mainContainer.innerHTML += `<div class="card position-relative">
+                <img src="${(url)}" alt="first image">
+                <p class="fst-italic">${(title)}</p>
+                <img class="pin position-absolute top-0 start-50 translate-middle" src="img/img/pin.svg" alt="immagine numero ${(id)}">
+            </div>`
+        });
+
+    })
+    .catch(error => {
+        console.error(error)
+    })
